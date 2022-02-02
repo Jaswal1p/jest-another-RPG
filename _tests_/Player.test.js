83dec499fdd97c7155a9944df36fc3641624891c
Jaswal1p <Jaswal1p@yahoo.com>
2022-02-02 01:10:03 -0500

    added prototype function

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -4,8 +4,6 @@ const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion.js');
 
-console.log(new Potion());
-
 test('creates a player object', () => {
     const player = new Player('Dave');
 
@@ -13,6 +11,7 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
